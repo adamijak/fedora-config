7024443bc6309c9b5255c2ab4cad0f17125c8279
adamijak <adamik.jakub72@gmail.com>
2021-10-20 19:53:20 +0200

    add new apps

---
+++ bottom/bottom.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+sudo dnf copr enable atim/bottom -y
+sudo dnf install bottom
---
+++ cisco-vpn/cisco-vpn.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+echo "Do not run as script. Execute commands interactively" && exit
+# Download and install libproidplus-gui-2.3.3-0.x86_64.rpm from https://proid.cz
+# then register it as p11 module
+echo /usr/lib64/libproidcm11.so > /usr/share/p11-kit/modules/proid.module
+
+# Trust certificates
+sudo trust anchor cair3.cer
+sudo trust anchor caiof3.cer
+
+# Comment insecure-hash = SHA1
+sudo gedit /etc/crypto-policies/back-ends/gnutls.config
+
+# Connect
+sudo openconnect -c '' https://vpn.csas.cz
